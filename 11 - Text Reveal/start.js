d23ecc5d6208c38159e0419f329b73423ebbbe8a
Ton Tosirikul <tontosirikul@Tons-MacBook-Pro.local>
2020-12-05 15:13:56 +0700

    add day 11 text reveal

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,4 +1,21 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function onScroll() {
+    const sectionElems = Array.from(document.querySelectorAll("section"));
+    sectionElems.forEach((sectionElem) => {
+      const imageElem = sectionElem.querySelector("img");
+      const textElem = sectionElem.querySelector(".text");
 
+      const scrollPosition = window.pageYOffset;
+      const revealPosition = imageElem.offsetTop + imageElem.offsetHeight / 10;
+
+      if (scrollPosition >= revealPosition) {
+        textElem.classList.add("reveal");
+      }
+    });
+  }
+  function run() {
+    document.addEventListener("scroll", onScroll);
+  }
+  run();
 })();
