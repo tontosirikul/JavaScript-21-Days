82e41c9314d79c6a9db5977bbfd3ecd0d1ce5bd5
TonTosirikul <tonydude1999@gmail.com>
2020-12-10 15:22:07 +0700

    add day 16 text to speech

--- 16 - Text to Speech/start.js
+++ 16 - Text to Speech/start.js
@@ -1,4 +1,21 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const message = new SpeechSynthesisUtterance();
+  function onVoicesChanged() {
+    const voices = speechSynthesis.getVoices();
+    const thVoice = voices.find((voice) => voice.lang === "th-TH");
+    message.voice = thVoice;
+  }
 
+  function onClick(event) {
+    message.text = event.target.getAttribute("alt");
+    speechSynthesis.speak(message);
+  }
+  function run() {
+    speechSynthesis.addEventListener("voiceschanged", onVoicesChanged);
+    const ImgElems = Array.from(document.querySelectorAll("img"));
+    ImgElems.forEach((ImgElem) => ImgElem.addEventListener("click", onClick));
+  }
+
+  run();
 })();
